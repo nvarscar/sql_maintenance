4928997b128c4d61ce39e5c2a2a41349eaa2ad8f
Kirill Kravtsov <kravtsov.k@gmail.com>
2020-03-12 20:01:34 -0700

    environments

--- Invoke-Backup.ps1
+++ Invoke-Backup.ps1
@@ -1,4 +1,9 @@
-$servers = 'localhost','localhost:14333'
-foreach ($server in $servers) {
+# define environments
+$servers = @{
+    test = 'localhost','localhost:14333'
+    production = 'localhost:14333','localhost'
+}
+# initiate backups for a specific environment
+foreach ($server in $servers[$env:environment]) {
     Start-DbaAgentJob -SqlInstance $server -Job 'DatabaseBackup - USER_DATABASES - FULL' -Wait
 }
